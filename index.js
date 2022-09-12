863aa2778b6398b3ef9913e17bb0a0f57bded7f0
Dymond <dymond.hoffman03@gmail.com>
2022-09-12 01:03:08 -0700

    imported files

---
+++ index.js
@@ -0,0 +1,12 @@
+// Import generateHTML.js
+const generateHTML = require("./src/generateHTML");
+
+// Import classes
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+// Import modules
+const fs = require("fs");
+const inquirer = require("inquirer");
+
